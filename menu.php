ec6a0989e48b55ae44e3ba5404367d689210ee3f
Douglas Needham <cinnion@gmail.com>
2013-01-06 13:50:09 -0500

    Remove hostname from the URLs - Remove the 'http://isenfir.atlantia.sca.org/' prefix from the URLs in the   menu, as that not needed for same-site links.  It also means that   development can be done elsewhere and will continue to work.

--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
-<img src="http://isenfir.atlantia.sca.org//images/header_gold2.gif" width="133" height="120" alt="Shire of Isenfir, Society for Creative Anachronism" />
-<p><a id="menulink" href="http://isenfir.atlantia.sca.org/index.php">Home</a><br />
-<!--<a id="menulink" href="http://isenfir.atlantia.sca.org//calendar/home.php">Calendar</a><br />-->
-<a id="menulink" href="http://isenfir.atlantia.sca.org/newcomers/home.php">Newcomer Info</a><br />
-<a id="menulink" href="http://isenfir.atlantia.sca.org/officers.php">Officers and Policies</a><br />
-<a id="menulink" href="http://isenfir.atlantia.sca.org/directions/home.php">Directions &amp; Maps</a><br />
-<a id="menulink" href="http://isenfir.atlantia.sca.org/activities/home.php">Activities</a><br />
-<a id="menulink" href="http://isenfir.atlantia.sca.org/rmi/home.php">Red Mountain Inn</a><br />
-<a id="menulink" href="http://isenfir.atlantia.sca.org/mail.php">Email Lists</a><br />
-<a id="menulink" href="http://isenfir.atlantia.sca.org/links.php">Links of Interest</a><br />
+<img src="/images/header_gold2.gif" width="133" height="120" alt="Shire of Isenfir, Society for Creative Anachronism" />
+<p><a id="menulink" href="/index.php">Home</a><br />
+<!--<a id="menulink" href="//calendar/home.php">Calendar</a><br />-->
+<a id="menulink" href="/newcomers/home.php">Newcomer Info</a><br />
+<a id="menulink" href="/officers.php">Officers and Policies</a><br />
+<a id="menulink" href="/directions/home.php">Directions &amp; Maps</a><br />
+<a id="menulink" href="/activities/home.php">Activities</a><br />
+<a id="menulink" href="/rmi/home.php">Red Mountain Inn</a><br />
+<a id="menulink" href="/mail.php">Email Lists</a><br />
+<a id="menulink" href="/links.php">Links of Interest</a><br />
