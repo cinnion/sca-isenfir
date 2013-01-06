0ed74a4c27d43ca6e6dad665b3bba061f1b6ece0
Douglas Needham <cinnion@gmail.com>
2013-01-06 13:52:57 -0500

    Complete addition of contact info for Cinnion. - Complete adding my contact info as webminister.

--- officers.php
+++ officers.php
@@ -35,86 +35,89 @@
 
 <p>
 <h3>Seneschal</h3>
-<b>Robert Kuntz</b> (Dosso del Brachetto)<br>
-162 Pilot Mtn Hollow<br>
-Faber, VA 22938-2013<br>
-(434) 361-2263 (home)<br>
-(434) 989-4081<br>
-<a href="mailto:sallykuntz AT juno.com">sallykuntz AT juno.com</a><br>
+<b>Robert Kuntz</b> (Dosso del Brachetto)<br/>
+162 Pilot Mtn Hollow<br/>
+Faber, VA 22938-2013<br/>
+(434) 361-2263 (home)<br/>
+(434) 989-4081<br/>
+<a href="mailto:sallykuntz AT juno.com">sallykuntz AT juno.com</a><br/>
 
 
 <h3>Deputy Seneschal</h3>
-<b>David Oxford</b> (Bryan Morgan)<br>
-1850 Browns Gap Tpke<br>
-Charlottesville, VA 22901<br>
-(434) 823-2336 (home)<br>
-(434) 996-9131 (cell)<br>
-<a href="mailto:david AT oakhaven.org">david AT oakhaven.org</a><br>
+<b>David Oxford</b> (Bryan Morgan)<br/>
+1850 Browns Gap Tpke<br/>
+Charlottesville, VA 22901<br/>
+(434) 823-2336 (home)<br/>
+(434) 996-9131 (cell)<br/>
+<a href="mailto:david AT oakhaven.org">david AT oakhaven.org</a><br/>
 
 <h3>Chatelain/Castellan</h3>
-<b>Sally Kuntz</b> (Dolce dei Bracchi)<br>
-162 Pilot Mtn Hollow<br>
-Faber, VA 22938-2013<br>
-(434) 361-2263  (home)<br>
-<a href="mailto:sallykuntz AT juno.com">sallykuntz AT juno.com</a><br>
-<a href="mailto:castellan AT isenfir.org">castellan AT isenfir.org</a><br>
+<b>Sally Kuntz</b> (Dolce dei Bracchi)<br/>
+162 Pilot Mtn Hollow<br/>
+Faber, VA 22938-2013<br/>
+(434) 361-2263  (home)<br/>
+<a href="mailto:sallykuntz AT juno.com">sallykuntz AT juno.com</a><br/>
+<a href="mailto:castellan AT isenfir.org">castellan AT isenfir.org</a><br/>
 
 <h3>Deputy Chatelaine</h3>
-<b>Christine Stewart</b> (Cristofana di Lorenzo)<br>
-1501 Lanetown Road<br>
-Crozet, VA 22932<br>
-(434) 434-823-7733 (home)<br>
-(540) 887-3414 (work)<br>
-(434) 566-1143 (cell)<br>
-<a href="mailto:cometwine AT gmail.com">cometwine AT gmail.com</a><br>
-<a href="mailto:castellan AT isenfir.org">castellan AT isenfir.org</a><br>
+<b>Christine Stewart</b> (Cristofana di Lorenzo)<br/>
+1501 Lanetown Road<br/>
+Crozet, VA 22932<br/>
+(434) 434-823-7733 (home)<br/>
+(540) 887-3414 (work)<br/>
+(434) 566-1143 (cell)<br/>
+<a href="mailto:cometwine AT gmail.com">cometwine AT gmail.com</a><br/>
+<a href="mailto:castellan AT isenfir.org">castellan AT isenfir.org</a><br/>
 
 <h3>Exchequer</h3>
-<b>Bruce</b> <br>
+<b>Bruce</b> <br/>
 
 <h3>Deputy Exchequer</h3>
-<b>Christine Stewart</b> (Cristofana di Lorenzo)<br>
-1501 Lanetown Road<br>
-Crozet, VA 22932<br>
-(434) 434-823-7733 (home)<br>(540) 887-3414 (work)<br>
-(434) 566-1143 (cell)<br>
-<a href="mailto:cometwine AT gmail.com">cometwine AT gmail.com</a><br>
+<b>Christine Stewart</b> (Cristofana di Lorenzo)<br/>
+1501 Lanetown Road<br/>
+Crozet, VA 22932<br/>
+(434) 434-823-7733 (home)<br/>(540) 887-3414 (work)<br/>
+(434) 566-1143 (cell)<br/>
+<a href="mailto:cometwine AT gmail.com">cometwine AT gmail.com</a><br/>
 
 <h3>Knight Marshal</h3>
-<b>Scott Schultz</b> (Philip ap Griffith)<br>
-2259 Stony Point Rd<br>
-Charlottesville, VA 22911<br>
-434-293-3038<br>
-<a href="mailto:Scott AT GriffinsPerch.com">Scott AT GriffinsPerch.com</a><br>
+<b>Scott Schultz</b> (Philip ap Griffith)<br/>
+2259 Stony Point Rd<br/>
+Charlottesville, VA 22911<br/>
+434-293-3038<br/>
+<a href="mailto:Scott AT GriffinsPerch.com">Scott AT GriffinsPerch.com</a><br/>
 
 <h3>Thrown Weapons Marshal</h3>
-<b>Wayne Byers</b> (Gawain MacDonald)<br>
-Waynesboro, VA 22980<br>
-<a href="mailto:Waian1701 AT yahoo.com">Waian1701 AT yahoo.com</a><br>
+<b>Wayne Byers</b> (Gawain MacDonald)<br/>
+Waynesboro, VA 22980<br/>
+<a href="mailto:Waian1701 AT yahoo.com">Waian1701 AT yahoo.com</a><br/>
 
 <h3>Archery Marshal</h3>
-<b>Garth Groff</b> (Mungo Napier, Archer of Mallard Lodge)<br>
-Mallard Lodge<br>
-1713 Essex Road<br>
-Charlottesville, VA 22901<br>
-434-293-5700<br>
-434-924-3288<br>
-<a href="mailto:ggg9y AT virginia.edu">ggg9y AT virginia.edu</a><br>
+<b>Garth Groff</b> (Mungo Napier, Archer of Mallard Lodge)<br/>
+Mallard Lodge<br/>
+1713 Essex Road<br/>
+Charlottesville, VA 22901<br/>
+434-293-5700<br/>
+434-924-3288<br/>
+<a href="mailto:ggg9y AT virginia.edu">ggg9y AT virginia.edu</a><br/>
 
 <h3>Herald</h3>
-<b>Sally Kuntz</b> (Dolce dei Bracchi)<br>
-162 Pilot Mtn Hollow<br>
-Faber, VA 22938-2013<br>
-(434) 361-2263(home)<br>
-<a href="mailto:sallykuntz AT juno.com">sallykuntz AT juno.com</a><br>
+<b>Sally Kuntz</b> (Dolce dei Bracchi)<br/>
+162 Pilot Mtn Hollow<br/>
+Faber, VA 22938-2013<br/>
+(434) 361-2263(home)<br/>
+<a href="mailto:sallykuntz AT juno.com">sallykuntz AT juno.com</a><br/>
 
 <h3>Web Minister</h3>
-<b>Doug N</b> (Cinnion)<br>
-<a href="mailto:webminister AT isenfir.org">webminister AT isenfir.org</a><br>
+<b>Douglas Needham</b> (Cinnion map Liuelin map Higuel)<br/>
+477 Chestnut Ave<br/>
+Waynesboro VA 22980<br/>
+(540)471-9613<br/>
+<a href="mailto:webminister AT isenfir.org">webminister AT isenfir.org</a><br/>
 
 <h3>Deputy Web Minister</h3>
-<b>Kendra Schmid</b> (Nicolosa d'Isenfir)<br>
-<a href="mailto:knschmid AT gmail.com">webminister AT isenfir.org</a><br>
+<b>Kendra Schmid</b> (Nicolosa d'Isenfir)<br/>
+<a href="mailto:knschmid AT gmail.com">webminister AT isenfir.org</a><br/>
 
 </p>
 
