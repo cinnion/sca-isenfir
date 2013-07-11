168580cae6b6d58129b40aba9b39017b830953d3
Douglas Needham <cinnion@gmail.com>
2013-07-11 18:31:40 -0400

    Update page about subscribing to the mailing list - Remove old mailing list information - Add info about subscribing to the new list on scalists.org, including showing   a sample subscription reply.

--- mail.php
+++ mail.php
@@ -10,48 +10,77 @@
 
 <!-- Content Begins Here -->
 
-<ul>
-	<li><a href="#subscribe">How to Subscribe</a></li>
-	<li><a href="#debunk">Debunking Urban Legends and Hoaxes</a> </li>
-</ul>
-
 <h2>Introduction to the Lists</h2>
 
-<p>The Shire of Isenfir maintains two electronic mailing lists, one for group discussions and one for only announcements. These mailing lists are the primary means of communication for the group, and as such are very helpful in staying involved . The subjects of list messages include reminders about scheduled activities, last-minute site or time changes, general discussions of Shire policy, and links to useful information on the Web, as well as anything else members feel the need to discuss.</p>
+<p>The Shire of Isenfir recently shifted its mailing list from mailing lists hosted by the University of Virginia to a list hosted on a SCA specific maillist server.  This mailing list serves as the primary means of communication for the group between meetings, and as such are very helpful in staying involved . The subjects of list messages include reminders about scheduled activities, last-minute site or time changes, general discussions of Shire policy, and links to useful information on the Web, as well as anything else members feel the need to discuss.</p>
 
-<p>As stated above, there are TWO mailing lists for isenfir. The main list (isenfir AT virginia.edu) is for all general discussions that people want to have, and has an average message count of about 20-30 message per week (often fewer, occasionally more). The other list (isenfir-announce AT virginia.edu) is for announcements only, either reminders of scheduled events or information about time/site changes related to activities. This list has fewer than 5 messages per week, and is useful for those who just want to know what is going on without being involved in any general discussions. If you subscribe to the main list, you will be automatically subscribed to the announcements list as well. If you subscribe to only the announcements list, you will only receive mail for that list.</p>
-
-<p>The scope of the general mailing list is not rigidly defined, and in general the members are not terribly picky about what you post, so long as it is in some way relevant to the group or at least a number of people within the group. Items to generally avoid, though, are chain letters, mundane politic issues or petitions (ones that don't impact the SCA or Isenfir in some way), virus warnings, and general commercial or non-commercial &quot;spam&quot;. Basically, before posting or forwarding something non-SCA, ask yourself if it is REALLY something EVERYONE on the mailing list will be interested in. And if it's something &quot;politically urgent&quot; or otherwise something you &quot;must forward immediately, to everyone you know, or else the world will end!&quot; please check out it's validity at one of the &quot;debunking&quot; sites listed below.</p>
+<p>The scope of the general mailing list is not rigidly defined, and in general the members are not terribly picky about what you post, so long as it is in some way relevant to the group or at least a number of people within the group. Items to generally avoid, though, are chain letters, mundane politic issues or petitions (ones that don't impact the SCA or Isenfir in some way), virus warnings, and general commercial or non-commercial &quot;spam&quot;. Basically, before posting or forwarding something non-SCA, ask yourself if it is REALLY something EVERYONE on the mailing list will be interested in. And if it's something &quot;politically urgent&quot; or otherwise something you &quot;must forward immediately, to everyone you know, or else the world will end!&quot; please check out it's validity at one of the many &quot;debunking&quot; sites found on the Internet.</p>
 
 <h2><a name="subscribe"></a>How to Subscribe</h2>
-<P>Go to the Sympa mailing list server <A HREF="http://lists.virginia.edu">http://lists.virginia.edu</A><img src="images/external_link.gif" title="External Link" alt="External Link" /> and log in using your UVa computing ID (click the "NetBadge" button at the top left) or Non-UVa email address.</P>
 
-<P>In the search field at the bottom of the Mailing List Categories block, enter "isenfir" (without quotes) and click the "Search Lists" block. This will bring up a list containing the "isenfir AT virginia.edu" and "isenfir-announce AT virginia.edu" mailing lists described above. Click the one you wish to subscribe to.</P>
+<p>Subscribing to the mailing list is as simple as:</p>
+<ol>
+<li>Go to <a href="http://atlantia.scalists.org/mailman/listinfo/isenfir_atlantia.scalists.org" target="_blank">The Mailman list server page for Isenfir</a></li>
+<li>In the section entitled ``Subscribing to Isenfir'', enter your email address, your name, and optionally pick a password, and pick whether you want to receive messages individually, or in a batched digest.<br/>
+    <br/>
+    While it says the name is optional, <b>PLEASE</b> enter your <b>SCA name</b> (or whatever everyone knows you by) into the form. This is important because it makes it easier for everyone to recognize that the request, email, etc. is from you, rather than some mostly meaningless mail address.<br/>
+    <br/>
+    Also, whether you enter it, or request that one be randomly generated, the password allows access to do things such changing your subscription to/from digest, etc.  If you choose a password rather than have one be generated for you, <b>PLEASE DO NOT</b> reuse a password which you use anywhere else.  These passwords are stored in a format which enables them to be mailed to you as a reminder, and should the worst thing occur, password reuse could open up multiple accounts to being compromised. (This would still be true even if they were stored in an extremely secure fashion, as passwords do eventually yield to brute force attacks, even if they are encrypted).<br/>
+    <br/></li>
+<li>Now, await for your subscription being approved, at which point, you will receive your welcome message with information about the list and how to post, change your settings, etc.  The message will look something like this:<br/>
+    <hr/>
+    <pre>From: &lt;isenfir-request@atlantia.scalists.org&gt;
+Date: Wed, May 15, 2013 at 11:42 PM
+Subject: Welcome to the "Isenfir" mailing list
+To: your-email-address@here
 
-<P>At the information page for the list you have chosen, click "Subscribe" near the bottom of the left-hand column. Please be patient as response on the server can be quite slow.</P>
 
-<P>For assistance, questions or comments, please contact the list owner at <A HREF="mailto:hlf AT virginia.edu"> hlf AT virginia.edu</A> or <A HREF="mailto:webminister AT isenfir.org"> webminister AT isenfir.org</A>.
+Welcome to the Isenfir@atlantia.scalists.org mailing list! Greetings
+and welcome to the mailing list for the SCA Shire of Isenfir, in the
+Kingdom of Atlantia.  Isenfir covers the independent cities of
+Charlottesville, Harrisonburg, Staunton and Waynesboro, as well as the
+counties of Albemarle, Greene, Madison, Nelson, Orange and Rockingham
+in Virginia. More information about the shire can be found at
+http://isenfir.atlantia.sca.org
 
-<h2><a name="debunk"></a>Debunking Urban Legends and Hoaxes</h2>
+To post to this list, send your message to:
 
-<p>It is an unfortunate fact that there are many, many false and misleading stories circulating via e-mail around the Internet. Some were originally well-intended, some were just jokes gone awry, and some are downright malicious. In most cases, the message will urge you to &quot;forward this message to everyone you know&quot; or something similar to ensure it's duplication. In general, do NOT forward messages to mailing lists that in any way appear to be chain letters or other messages designed to &quot;keep going&quot; (even the &quot;cute&quot; ones like &quot;send this message to 10 friends to let them know you love them, and you'll get lots of love in return&quot; ... if you want to send it to 10 personal addresses, that's your business, but DON'T send it to a mailing list). If you think the information in the message is just too important not to send to the mailing list, please verify it's authenticity before sending it. There really are LOTS of hoaxes out there, and they just keep going around and around so long as people forward them. The links below are to several sites that will help you determine the validity of a message. Please utilize them before forwarding any sort of &quot;warning&quot; or &quot;call to action&quot; to a mailing list, especially this one! </p>
+  isenfir@atlantia.scalists.org
 
-<ul>
-	<li><a href="http://www.snopes.com/inboxer/inboxer.htm">Urban Legends Reference Pages: Inboxer Rebellion</a><img src="images/external_link.gif" title="External Link" alt="External Link" /> (specifically geared toward e-mail hoaxes -- has a search feature)</li>
-	<li><a href="http://www.snopes.com">Urban Legends Reference Pages</a><img src="images/external_link.gif" title="External Link" alt="External Link" /> (main page of above site -- has information on all manner of urban legends)</li>
-	<li><a href="http://www.nonprofit.net/hoax/hoax.html">Don't Spread That Hoax!</a><img src="images/external_link.gif" title="External Link" alt="External Link" />
-</ul>
+General information about the mailing list is at:
 
-<p>If you get some warning about some outrageous piece of legislation currently up for consideration before Congress, check it out at this Library of Congress site: <a
-href="http://thomas.loc.gov/">THOMAS -- U.S. Congress on the Internet</a><img src="images/external_link.gif" title="External Link" alt="External Link" /></p>
+  http://atlantia.scalists.org/mailman/listinfo/isenfir_atlantia.scalists.org
 
-<p>If you get a virus warning, check it out at these sites:
 
-<ul>
-  <li><a href="http://kumite.com/myths/">Computer Virus Myths home page</a><img src="images/external_link.gif" title="External Link" alt="External Link" /></li>
-  <li><a href="http://www.sarc.com/">Symantec AntiVirus Research Center</a><img src="images/external_link.gif" title="External Link" alt="External Link" /></li>
-  <li><a href="http://www.mcaffee.com/centers/anti-virus/">McAfee.com - Anti-Virus Center</a><img src="images/external_link.gif" title="External Link" alt="External Link" /></li>
-</ul>
+If you ever want to unsubscribe or change your options (eg, switch to
+or from digest mode, change your password, etc.), visit your
+subscription page at:
+
+  http://atlantia.scalists.org/mailman/options/isenfir_atlantia.scalists.org/your-email-address@here
+
+
+You can also make such adjustments via email by sending a message to:
+
+  Isenfir-request@atlantia.scalists.org
+
+with the word `help' in the subject or body (don't include the
+quotes), and you will get back a message with instructions.
+
+You must know your password to change your options (including changing
+the password, itself) or to unsubscribe without confirmation.  It is:
+
+  xyzzyplugh
+
+Normally, Mailman will remind you of your atlantia.scalists.org
+mailing list passwords once every month, although you can disable this
+if you prefer.  This reminder will also include instructions on how to
+unsubscribe or change your account options.  There is also a button on
+your options page that will email your current password to you.</pre>
+    <hr/>
+    <br/>
+    </li>
+<li>Once you have this email message, you can start posting to the list by sending your messages to <a mailto:isenfir@atlantia.scalists.org" target="_blank">isenfir@atlantia.scalists.org</a>. You can also visit your subscription page to make changes, unsubscribe, etc., or follow the other hints provided in the example mail message shown above.</li>
+</ol>
 
 <!-- Content Ends Here -->
 
