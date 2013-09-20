85327690f6007177471e9cc86bedeb548ec973ae
Douglas Needham <cinnion@gmail.com>
2013-09-20 15:06:35 -0400

    Beginnings of a page for a new event. - Add a "More information coming soon" page for the Gemutlichplatz event, so   that it exists for fliers and the like.

---
+++ events/gemutlich.php
@@ -0,0 +1,23 @@
+<html><head>
+    <title>Gem&uuml;tlichplatz</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/default.css" />
+</head>
+<body>
+    <table width="100%" border="0"><tr valign="top">
+    <td width="20%" id="Menu"><? include "../menu.php"; ?></td>
+    <td id="Content">
+            <h1>Gem&uuml;tlichplatz</h1>
+
+<!-- Content Begins Here -->
+        <p>More information coming soon!</p>
+
+<!-- Content Ends Here -->
+
+</tr></table>
+<? include "../footer.php"; ?>
+</body>
+</html>
+
+    
+
+
