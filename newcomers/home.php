4c877b34e89dd95f939cd58652644f00be7f4aec
Douglas Needham <cinnion@gmail.com>
2013-09-20 14:56:46 -0400

    Centralize the Google Analytics code into the footer - Move the Google Analytics code into footer.php, so that it is included on   all pages which use the footer already,  Placement by the footer also means   that it is placed where the GA documentation states it should be.

--- newcomers/home.php
+++ newcomers/home.php
@@ -1,20 +1,6 @@
 <html><head>
 	<title>Newcomer Information</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="../default.css" />
-	
-	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-87291-12']);
-		_gaq.push(['_trackPageview']);
-
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-
-	</script>
-	
 </head><body>
 <table width="100%" border="0"><tr valign="top">
 <td width="20%" id="Menu"><? include "../menu.php"; ?></td>
