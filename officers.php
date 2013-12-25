f2c4d9dda03043e83edfba1d24a7a855039fe584
Douglas Needham <cinnion@gmail.com>
2013-12-25 14:41:34 -0500

    Officer page updates - Update all email addresses to forwarding addresses at isenfir.atlantia.sca.org - Change in office for herald.

--- officers.php
+++ officers.php
@@ -26,7 +26,7 @@
 Faber, VA 22938-2013<br/>
 (434) 361-2263 (home)<br/>
 (434) 989-4081<br/>
-<a href="mailto:sallykuntz AT juno.com">sallykuntz AT juno.com</a><br/>
+<a href="mailto:seneschal AT isenfir.atlantia.sca.org">seneschal AT isenfir.atlantia.sca.org</a><br/>
 
 
 <h3>Deputy Seneschal</h3>
@@ -35,15 +35,14 @@ Faber, VA 22938-2013<br/>
 Charlottesville, VA 22901<br/>
 (434) 823-2336 (home)<br/>
 (434) 996-9131 (cell)<br/>
-<a href="mailto:david AT oakhaven.org">david AT oakhaven.org</a><br/>
+<a href="mailto:seneschal-deputy AT isenfir.atlantia.sca.org">seneschal-deputy AT isenfir.atlantia.sca.org</a><br/>
 
 <h3>Chatelain/Castellan</h3>
 <b>Sally Kuntz</b> (Dolce dei Bracchi)<br/>
 162 Pilot Mtn Hollow<br/>
 Faber, VA 22938-2013<br/>
 (434) 361-2263  (home)<br/>
-<a href="mailto:sallykuntz AT juno.com">sallykuntz AT juno.com</a><br/>
-<a href="mailto:castellan AT isenfir.org">castellan AT isenfir.org</a><br/>
+<a href="mailto:castellan AT isenfir.atlantia.sca.org">castellan AT isenfir.atlantia.sca.org</a><br/>
 
 <h3>Deputy Chatelaine</h3>
 <b>Christine Stewart</b> (Cristofana di Lorenzo)<br/>
@@ -52,8 +51,7 @@ Crozet, VA 22932<br/>
 (434) 434-823-7733 (home)<br/>
 (540) 887-3414 (work)<br/>
 (434) 566-1143 (cell)<br/>
-<a href="mailto:cometwine AT gmail.com">cometwine AT gmail.com</a><br/>
-<a href="mailto:castellan AT isenfir.org">castellan AT isenfir.org</a><br/>
+<a href="mailto:castellan AT isenfir.atlantia.sca.org">castellan AT isenfir.atlantia.sca.org</a><br/>
 
 <h3>Exchequer</h3>
 <b>Bruce</b> <br/>
@@ -64,19 +62,21 @@ Crozet, VA 22932<br/>
 Crozet, VA 22932<br/>
 (434) 434-823-7733 (home)<br/>(540) 887-3414 (work)<br/>
 (434) 566-1143 (cell)<br/>
-<a href="mailto:cometwine AT gmail.com">cometwine AT gmail.com</a><br/>
+<a href="mailto:exchequer AT isenfir.atlantia.sca.org">exchequer AT isenfir.atlantia.sca.org</a><br/>
 
 <h3>Knight Marshal</h3>
 <b>Scott Schultz</b> (Philip ap Griffith)<br/>
 2259 Stony Point Rd<br/>
 Charlottesville, VA 22911<br/>
 434-293-3038<br/>
+<a href="mailto:marshal-heavy AT isenfir.atlantia.sca.org">marshal-heavy AT isenfir.atlantia.sca.org</a><br/>
+
 <a href="mailto:Scott AT GriffinsPerch.com">Scott AT GriffinsPerch.com</a><br/>
 
 <h3>Thrown Weapons Marshal</h3>
 <b>Wayne Byers</b> (Gawain MacDonald)<br/>
 Waynesboro, VA 22980<br/>
-<a href="mailto:Waian1701 AT yahoo.com">Waian1701 AT yahoo.com</a><br/>
+<a href="mailto:marshal-thrown AT isenfir.atlantia.sca.org">marshal-thrown AT isenfir.atlantia.sca.org</a><br/>
 
 <h3>Archery Marshal</h3>
 <b>Garth Groff</b> (Mungo Napier, Archer of Mallard Lodge)<br/>
@@ -85,21 +85,22 @@ Mallard Lodge<br/>
 Charlottesville, VA 22901<br/>
 434-293-5700<br/>
 434-924-3288<br/>
-<a href="mailto:ggg9y AT virginia.edu">ggg9y AT virginia.edu</a><br/>
+<a href="mailto:marshal-archery AT isenfir.atlantia.sca.org">marshal-archery AT isenfir.atlantia.sca.org</a><br/>
 
 <h3>Herald</h3>
-<b>Karolyn McNeely</b> (Elena Modarova vnuka)<br/> <!-- Concent updated 2013 Jan 7 -->
-131 Crompton Road<br/>
+<b>Willow Needham</b> (Joie la bedelle)<br/> <!-- Concent updated 2013 Jan 7 -->
+477 Chestnut Ave<br/>
 Waynesboro VA 22980<br/>
-540-471-1401 (home - Emergencies only between 12am and 8am - texting encouraged anytime)<br/>
-<a href="mailto:dorathalia AT yahoo.com">dorathalia AT yahoo.com</a><br/>
+540-471-1158<br/>
+<a href="mailto:herald AT isenfir.atlantia.sca.org">herald AT isenfir.atlantia.sca.org</a><br/>
+
 
 <h3>Web Minister</h3>
 <b>Douglas Needham</b> (Cinnion map Liuelin map Higuel)<br/> <!-- Concent given 2012 July 1 -->
 477 Chestnut Ave<br/>
 Waynesboro VA 22980<br/>
 (540)471-9613 (cell - Emergencies only between 12am and 8am - texting encouraged anytime)<br/>
-<a href="mailto:cinnion AT gmail.com">cinnion AT gmail.com</a><br/>
+<a href="mailto:webminister AT isenfir.atlantia.sca.org">webminister AT isenfir.atlantia.sca.org</a><br/>
 
 <h3>Deputy Web Minister</h3>
 <b>Kendra Schmid</b> (Nicolosa d'Isenfir)<br/>
